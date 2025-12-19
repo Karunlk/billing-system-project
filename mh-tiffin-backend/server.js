74683ab21361ef9a956aa6cecfdb0ea4d27d20bf
dummy-coder-man <dummycontent91@gmail.com>
2025-12-20 00:20:25 +0530

    Update server.js

--- mh-tiffin-backend/server.js
+++ mh-tiffin-backend/server.js
@@ -8,7 +8,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ['https://billing-system-xi-khaki.vercel.app', 'http://localhost:3000'],
+  origin: "*",
   credentials: true
 }));
 app.use(bodyParser.json({ limit: '50mb' }));
